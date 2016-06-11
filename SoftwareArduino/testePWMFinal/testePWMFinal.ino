ff7ea8b8325339f94495052f88c56053668bed12
bileki <ctrlcctrlvaccount@gmail.com>
2016-06-10 21:45:57 -0300

    dados do carro

--- SoftwareArduino/testePWMFinal/testePWMFinal.ino
+++ SoftwareArduino/testePWMFinal/testePWMFinal.ino
@@ -1,42 +1,15 @@
 #include <SoftwareSerial.h>
 #include <PWM.h>
 
-#define LED 13
-#define LASTERRORINI 100000
-#define LIMTOP -10
-#define LIMDOWN 5
-#define LIMDELTA 100
-#define SUMMAX 10
-#define INTERACTIONS 10
-#define RESPONSETIME 5
-#define TOL 1
-#define FAIXA 5
-#define TOLKI 15
+
 #define RxD 7
 #define TxD 8
 #define pinServo 3
-#define Reset 10
 
-float lastProcess = 0;
-float lastError = LASTERRORINI;
-float kp = 0.06, ki = 0.003, kd = 0.0, kb = 0.3;
-float sum = 0;
-float servo = 0, pid = 0;
 int pwm = 0;
-int targetVelocity = 100, lastTargetVelocity = 100;
-int velocityBand = 100, velocity = 0, velocityMedia = 0;
-int countInteractions = 0, count = 0;
-
-// Flags de controle
-boolean goingUp = true;
-boolean flagLight = false;
-boolean flagBreak = true;
-boolean flagPID = false;
-boolean flagActivatePID = false;
-
-// Variáveis para controlar o PWM
 int32_t frequency = 150;
 bool success = false;
+bool flagT = true;
 
 
 SoftwareSerial mySerial(RxD, TxD);
@@ -49,102 +22,12 @@ int flag = 0, sair = 0;
 
 unsigned int throttle, rpm;
 
-float PID() {
-  float P, I, D, B, myPID, dt, error;
-  float breaker = 0, delta = 0;
-
-  error = targetVelocity - velocity;
-
-  dt = (millis() - lastProcess) / 1000.0;
-
-  P = kp * error;
-
-  sum += (error * dt);
-  I = ki * sum;
-
-  if ((dt > 0) && (lastError != 100000)) {
-    delta = ((error - lastError) / dt);
-    D = kd * delta;
-  }
-  else 
-    D = delta = 0;
-
-  if (((error > 0) && (lastError < 0)) || ((error < 0) && (lastError > 0)) || (error == 0) || (abs(delta) < LIMDELTA))
-    flagBreak = false;
-
-  if ((targetVelocity != lastTargetVelocity))
-    flagBreak = true;
-
-  if ((targetVelocity - lastTargetVelocity) > 0)
-    goingUp = true;
-  if ((targetVelocity - lastTargetVelocity) < 0)
-    goingUp = false;
-
-  lastTargetVelocity = targetVelocity;
-
-  if ((flagBreak) && (error != 0))
-    if (goingUp) breaker = -1 * abs(delta / (error * error));
-    else breaker = abs(delta / (error * error));
-  else
-    breaker = 0;
-
-  B = kb * breaker;
-
-  if(B > LIMDOWN)
-    B = LIMDOWN;      
-  if(B < LIMTOP)
-    B = LIMTOP;       
-  
-  lastError = error;
-  lastProcess  = millis();
-
-  myPID = P + I + D + B;
-  
-  return myPID;
-}
-
-
-
-void IA() {
-  if (!flagPID) {
-    if ((velocity < (velocityBand + FAIXA)) && (velocity > (velocityBand - FAIXA))) {
-      countInteractions++;
-    } else {
-      countInteractions = 0;
-      velocityBand = velocity;
-    }
-    if (countInteractions > INTERACTIONS) {
-      digitalWrite(LED, HIGH);
-      flagLight = true;
-      targetVelocity = velocity;
-      servo = pwm;        
-    }
-    if (flagLight) {
-      if (count > 3) { 
-        flagActivatePID = true;
-        flagPID = true;
-      } else {
-        sum = 0;
-      }
-      count++;
-    }
-  } else {
-    if (throttle > targetVelocity) {
-      flagPID = false;
-      flagActivatePID = false;
-      digitalWrite(LED, LOW);
-      flagLight = false;
-      countInteractions = 0;
-      count = 0;
-      velocityBand = velocity;
-    }
-  }
-}
-
-
 void rpm_calc() {
   boolean valid;  
   int i;
+
+  Serial.println("check rpm resp");
+  Serial.println(check);
 
   valid=false;
 
@@ -172,6 +55,8 @@ void rpm_calc() {
 
 void throttle_calc(){
   boolean valid;  
+  Serial.println("check tr resp");
+  Serial.println(check);
 
   valid=false;
 
@@ -223,7 +108,6 @@ void getThrottle() {
     throttle_calc();
     check = "";
     mySerial.flush(); 
-    mySerial.write("0111\r\n");
     flag = 0;
   }
 }
@@ -241,7 +125,6 @@ void getRPM() {
     rpm_calc();
     check = "";
     mySerial.flush(); 
-    mySerial.write("010C\r\n");
     flag = 0;
   }
 }
@@ -263,21 +146,6 @@ void resposta() {
 
 void pareiaBT() {
   while (1) {
-    if (mySerial.available()) {  
-      delay(100);  
-      while (mySerial.available() > 0) {
-        c = mySerial.read();
-        check += c;
-        flag = 1;
-      }
-      if(flag==1) {
-        Serial.print(check);
-        Serial.write("\n");
-        throttle_calc();
-        check = "";
-        flag = 0;
-      }
-    }
 
     if (Serial.available()) {  
       delay(100);  
@@ -378,56 +246,21 @@ void setup() {
 }
 
 void loop() {  
-
   if (success) {
-    getThrottle();
-    getRPM();
-    velocity = rpm;
-    IA();
-    pid = PID(); 
-
-    if (flagLight) {  
-      if (flagActivatePID)  
-        servo += pid;      
-
-      if (servo > 90)
-        servo = 90;
-      if (servo < 30)
-        servo = 30;
-
-      if ((velocity > (targetVelocity + TOLKI)) || (velocity < (targetVelocity - TOLKI))) 
-        sum = 0;
-
-      if (sum > SUMMAX)
-        sum = SUMMAX;
-      if (sum < -SUMMAX)
-        sum = -SUMMAX;
-
-      pwm = (int)servo;
-
+    if (flagT) {
+      flagT = false;
+      getThrottle();
     } else {
-      pwm = throttle;
+      flagT = true;
+      getRPM();
     }
 
+    
     // Debugging...
     Serial.print(throttle);
     Serial.print("\t");
-    Serial.print(targetVelocity);
-    Serial.print("\t");
-    Serial.print(velocity);      
-    Serial.print("\t");
-    Serial.print(velocityBand);      
-    Serial.print("\t");
-    Serial.print(pwm);
-    Serial.print("\t");
-    Serial.print(sum);
-    Serial.print("\t");
-    Serial.print(countInteractions);
-    Serial.print("\t");
-    Serial.print(flagLight);
-    Serial.print("\t");
+    Serial.print(rpm);
     Serial.println();
-
   }
 
   delay(300);
